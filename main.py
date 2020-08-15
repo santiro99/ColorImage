820f790b24c458beffa47a02955f61fdfdd575d8
santiro99 <69703674+santiro99@users.noreply.github.com>
2020-08-14 21:11:30 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from colorImage import *
+import cv2
+
+def print_hi(name):
+    print(f'Hi, {name}') 
+
+if __name__ == '__main__':
+    path = input("Ingrese la ruta de la imagen con la que desea trabajar: ")  #/Users/santiagorojasjaramillo/Downloads/lena.png
+    ejercicio = colorImage(path)
+    ejercicio.displayProperties()
+    ejercicio.makeGray()
+    ejercicio.colorizeRGB('red')
+    ejercicio.makeHue()
