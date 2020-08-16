d0eb647b7ee505c5b68b86fe933610003933cfc9
Santiago Rojas Jaramillo <69703674+santiro99@users.noreply.github.com>
2020-08-16 17:11:43 -0500

    Update colorImage.py

--- colorImage.py
+++ colorImage.py
@@ -1,5 +1,4 @@
 import cv2
-import numpy as np
 
 class colorImage:
     def __init__(self, path):                         #Consctructor de la clase, con parámetro de entrada: path de la imagen.
@@ -19,29 +18,23 @@ class colorImage:
         cv2.waitKey(0)
 
     def colorizeRGB(self, color):
-        canales_zeros = np.zeros(self.propiedades)                                                 #Crea matriz de ceros del mismo tamaño y dimensiones de la imagen que se trabaja
-        if color == 'red':                                                                         #En caso de que la entrada al método sea red, realiza el proceso de imagen rojiza.
-            canal_rojo = self.image[:, :, 2]                                                       #Se extrae el canal rojo de la imagen original
-            canales_zeros[:, :, 2] = canal_rojo                                                    #En el canal rojo de la matriz de ceros, se asigna el canal rojo de la imagen original
-            cv2.imwrite("/Users/santiagorojasjaramillo/Downloads/Red_image.png", canales_zeros)    #Guarda la imagen roja en el path indicado
-            rojiza = cv2.imread('/Users/santiagorojasjaramillo/Downloads/Red_image.png')           #Lee la imagen guardada y la almacena en la variable rojiza.
-            cv2.imshow('Imagen_Rojiza', rojiza)                                                    #Muestra la imagen rojiza.
+        imagenColorize = self.imageColorize
+        if color == 'red':                                      # En caso de que la entrada al método sea red, realiza el proceso de imagen rojiza.
+            imagenColorize[:, :, 1] = 0                         #Canal 1 (verde) es fijada en 0.
+            imagenColorize[:, :, 0] = 0                         #Canal 0 (azul) es fijada en 0.
+            cv2.imshow('Imagen_Rojiza', imagenColorize)         #Muestra la imagen rojiza.
             cv2.waitKey(0)
 
-        if color == 'green':                                                                       #Realiza el mismo proceso del color rojo pero con verde.
-            canal_verde = self.image[:, :, 1]
-            canales_zeros[:, :, 1] = canal_verde
-            cv2.imwrite("/Users/santiagorojasjaramillo/Downloads/Green_image.png", canales_zeros)
-            verdoza = cv2.imread('/Users/santiagorojasjaramillo/Downloads/Green_image.png')
-            cv2.imshow('Imagen_Verdoza', verdoza)
+        if color == 'green':                                    #Realiza el mismo proceso del color rojo pero con verde.
+            imagenColorize[:, :, 2] = 0
+            imagenColorize[:, :, 0] = 0
+            cv2.imshow('Imagen_Verdoza', imagenColorize)
             cv2.waitKey(0)
 
-        if color == 'blue':                                                                        ##Realiza el mismo proceso del color rojo pero con azul.
-            canal_azul = self.image[:, :, 0]
-            canales_zeros[:, :, 0] = canal_azul
-            cv2.imwrite("/Users/santiagorojasjaramillo/Downloads/Blue_image.png", canales_zeros)
-            azuloza = cv2.imread('/Users/santiagorojasjaramillo/Downloads/Blue_image.png')
-            cv2.imshow('Imagen_Azuloza', azuloza)
+        if color == 'blue':                                     #Realiza el mismo proceso del color rojo pero con azul.
+            imagenColorize[:, :, 2] = 0
+            imagenColorize[:, :, 1] = 0
+            cv2.imshow('Imagen_Azuloza', imagenColorize)
             cv2.waitKey(0)
 
     def makeHue(self):
@@ -49,5 +42,5 @@ class colorImage:
         HSV[1] = 255                                              #Componente S es fijada en 255 constante.
         HSV[2] = 255                                              #Componente V es fijada en 255 constante
         BGR_again = cv2.cvtColor(self.image, cv2.COLOR_HSV2BGR)   #Devuelve la imagen a espacio de color RGB y la guarda en variable.
-        cv2.imshow('Hue', BGR_again)                              #Muestra imagen modificada en espacio RGB.                     
+        cv2.imshow('Hue', BGR_again)                              #Muestra imagen modificada en espacio RGB.
         cv2.waitKey(0)
