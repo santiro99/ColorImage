806f7034f4083f2c5be3d6460ab8fd4cab9a4cd8
Santiago Rojas Jaramillo <69703674+santiro99@users.noreply.github.com>
2020-08-16 17:01:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from colorImage import *
-import cv2
 
 if __name__ == '__main__':
     path = input("Ingrese la ruta de la imagen con la que desea trabajar: ")  #Se pide al usuario ingresar path de imagen a trabajar.
